84a40b2c8c465663995da796b84f47e62b5dce46
Administrator <2814126219@qq.com>
2025-10-13 10:16:14 +0800

    style: 移除drawer bottom bar中的hide drawer按钮

--- composeApp/src/commonMain/kotlin/com/sky31/gongmultiplatform/ui/component/drawer/DrawerBottomBar.kt
+++ composeApp/src/commonMain/kotlin/com/sky31/gongmultiplatform/ui/component/drawer/DrawerBottomBar.kt
@@ -5,17 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,10 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sky31.gongmultiplatform.di.LocalNavController
 import com.sky31.gongmultiplatform.ui.viewModel.AuthViewModel
-import gongmultiplatform.composeapp.generated.resources.Res
-import gongmultiplatform.composeapp.generated.resources.baseline_arrow_back_24
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun DrawerBottomBar(
@@ -68,35 +59,6 @@ fun DrawerBottomBar(
                 color = MaterialTheme.colorScheme.onPrimary,
                 style = MaterialTheme.typography.labelMedium
             )
-        }
-
-        Spacer(
-            modifier = Modifier
-                .width(10.dp)
-        )
-
-        IconButton(
-            onClick = {
-                scope.launch {
-                    state.close()
-                }
-            }
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(50))
-                    .background(MaterialTheme.colorScheme.primary),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    painter = painterResource(Res.drawable.baseline_arrow_back_24),
-                    contentDescription = "back",
-                    tint = MaterialTheme.colorScheme.onPrimary,
-                    modifier = Modifier
-                        .size(28.dp)
-                )
-            }
         }
     }
 }
