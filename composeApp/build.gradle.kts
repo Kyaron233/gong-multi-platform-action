19922bae233ac40aa8b2858890b2db640951f459
Administrator <2814126219@qq.com>
2025-10-30 19:36:41 +0800

    chore(release): 更新版本号为3.1.20251030-beta

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -104,7 +104,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "3.1.20251001-beta"
+        versionName = "3.1.20251030-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     packaging {
